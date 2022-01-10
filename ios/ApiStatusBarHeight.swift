2c0c58155bfe1e5f8dbf635656c2e6779fb1bb47
Phạm Văn Sơn <vanson9x.it@gmail.com>
2022-01-11 00:13:11 +0700

    YellowBox
    Main Thread Checker: UI API called on a background thread

--- ios/ApiStatusBarHeight.swift
+++ ios/ApiStatusBarHeight.swift
@@ -1,19 +1,20 @@
 @objc(ApiStatusBarHeight)
 class ApiStatusBarHeight: NSObject {
-
     @objc static func requiresMainQueueSetup() -> Bool {
         return false
     }
-    
+
     @objc(getStatusBarHeight:withRejecter:)
     func getStatusBarHeight(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        var statusBarHeight: CGFloat = 0
-        if #available(iOS 13.0, *) {
-            let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-            statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
-        } else {
-            statusBarHeight = UIApplication.shared.statusBarFrame.height
+        DispatchQueue.main.sync {
+            var statusBarHeight: CGFloat = 0
+            if #available(iOS 13.0, *) {
+                let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+                statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+            } else {
+                statusBarHeight = UIApplication.shared.statusBarFrame.height
+            }
+            resolve(statusBarHeight)
         }
-        resolve(statusBarHeight)
     }
 }
